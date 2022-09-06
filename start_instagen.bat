94846608589b9771fa0d3f46aefe5ec1f0d3e3ab
WieszakWare <wieszaki15@gmail.com>
2022-09-06 21:09:43 +0200

    start.bat Changed
    */ ChangeLog:
    1. Updated code.
    2. Better Design.
    3. Auto install python libraries.

--- start_instagen.bat
+++ start_instagen.bat
@@ -1,5 +1,8 @@
 @echo off
-title SETUP
+title InstaGen SETUP / InstaGen / .py / Source Code Setup / WieszakWare / 2022 / .bat / GitHub
+color 2
+echo Installing Requirements [ Important Python Libratries ]
 pip install -r requirements.txt
+echo Attemting to run main.py
 python main.py
-pause < null
+pause
